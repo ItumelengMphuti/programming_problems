6eeea4f08422c41463eaf41581c02c8304f567f2
ItumelengMphuti <itumeleng1mphuti@gmail.com>
2025-03-12 16:15:33 +0200

    New Challenges

--- Upskilling/last_char_equals_to_target.js
+++ Upskilling/last_char_equals_to_target.js
@@ -1,5 +1,6 @@
 function confirmEnding(str, target) {
-    return str.slice(-target.length) === target //start slicing from the position that is target.length characters from the end of the string.
+    return str.slice(-target.length) === target //start slicing from the position that is target.length characters 
+                                                //from the end of the string.
   }
   
   console.log(confirmEnding('Itumeleng', 'en'));
